72dce7b861d31b04ed85287f911268a67264b545
IMALIA ROSYIDA <222112106@stis.ac.id>
2026-06-17 11:02:45 +0700

    fix: package.json

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: [
+    "192.168.80.108",
+  ],
+};
 
 export default withNextIntl(nextConfig);
